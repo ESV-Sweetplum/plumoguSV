0acdbe18dab51e4fdb7aaa0064585aea908678fc
tylersfoot <tylerdepa8@gmail.com>
2025-06-25 23:29:56 -0400

    hanate wakuso shiseo tadashite [QUA]  — 11:14 PM how to edit plumogu: step one: edit plugin.lua step two: save
    piano tiles king
    [FLX]
     — 11:14 PM
    o\hhh

---
+++ plugin.lua
@@ -0,0 +1,12 @@
+function draw()
+    meow()
+end
+
+function meow()
+    imgui.Begin("QWuavver polugin")
+    imgui.Text("Pluym")
+    quaver.MakeSVHappen()
+    quaver.CoolestSVmapeverCreateThatNowThanks()
+    quaver.submitEpicSVMapAndGetItRejectedFromRankingQueueForBeingTooEpic()
+    imgui.End()
+end
