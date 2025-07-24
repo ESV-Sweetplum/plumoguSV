9eadea62d5c6658aa51f8895119af82651df077d
ESV-Sweetplum <159661878+ESV-Sweetplum@users.noreply.github.com>
2025-07-24 15:33:00 -0700

    force tonumber to always return a number kekw

--- packages/math/toNumber.lua
+++ packages/math/toNumber.lua
@@ -3,6 +3,6 @@
 ---@return number
 function math.toNumber(x)
     local result = tonumber(x)
-    if (not result) then return 0 end
+    if (not result or type(result) ~= "number") then return 0 end
     return result
 end
