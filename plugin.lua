66ca1427c4d863c8d9138c8ff5ab340703f7c331
plummyyummy <sweetplumquaver@gmail.com>
2025-07-27 18:42:15 -0700

    optimize reactive singularities

--- plugin.lua
+++ plugin.lua
@@ -3118,7 +3118,12 @@ function renderReactiveSingularities()
     local dim = imgui.GetWindowSize()
     local sqrt = math.sqrt
     local clamp = math.clamp
-    local particles = state.GetValue("particles", {})
+    local xList = state.GetValue("xList", {})
+    local yList = state.GetValue("yList", {})
+    local vxList = state.GetValue("vxList", {})
+    local vyList = state.GetValue("vyList", {})
+    local axList = state.GetValue("axList", {})
+    local ayList = state.GetValue("ayList", {})
     local slowSpeedR = 89
     local slowSpeedG = 0
     local slowSpeedB = 255
@@ -3126,78 +3131,86 @@ function renderReactiveSingularities()
     local fastSpeedG = 165
     local fastSpeedB = 117
     if (dim.x < 100) then return end
-    if (not truthy(#particles)) then
-        createParticles(particles, 200)
+    if (not truthy(#xList)) then
+        createParticles(xList, yList, vxList, vyList, axList, ayList, 200)
     end
-    updateParticles(particles, dim, state.DeltaTime)
+    updateParticles(xList, yList, vxList, vyList, axList, ayList, dim, state.DeltaTime)
     local lerp = function(w, l, h)
         return w * h + (1 - w) * l
     end
-    for k = 1, #particles do
-        local p = particles[k]
-        local s = sqrt(p.vx ^ 2 + p.vy ^ 2)
+    for i = 1, #xList do
+        local x = xList[i]
+        local y = yList[i]
+        local vx = vxList[i]
+        local vy = vyList[i]
+        local ax = axList[i]
+        local ay = ayList[i]
+        local s = sqrt(vx ^ 2 + vy ^ 2)
         local clampedSpeed = clamp(s / 5, 0, 1)
         local r = lerp(clampedSpeed, slowSpeedR, fastSpeedR)
         local g = lerp(clampedSpeed, slowSpeedG, fastSpeedG)
         local b = lerp(clampedSpeed, slowSpeedB, fastSpeedB)
-        local pos = { p.x + topLeft.x, p.y + topLeft.y }
+        local pos = { x + topLeft.x, y + topLeft.y }
         ctx.AddCircleFilled(pos, 1.5,
             rgbaToUint(r, g, b, 255))
     end
     ctx.AddCircleFilled(dim / 2 + topLeft, 15, 4278190080)
     ctx.AddCircle(dim / 2 + topLeft, 16, 4294967295)
-    state.SetValue("particles", particles)
+    state.SetValue("xList", xList)
+    state.SetValue("yList", yList)
+    state.SetValue("vxList", vxList)
+    state.SetValue("vyList", vyList)
+    state.SetValue("axList", axList)
+    state.SetValue("ayList", ayList)
 end
-function createParticles(particles, n)
+function createParticles(x, y, vx, vy, ax, ay, n)
     local dim = imgui.GetWindowSize()
     for i = 1, n do
-        table.insert(particles, {
-            x = math.random() * dim.x,
-            y = math.random() * dim.y,
-            vx = 0,
-            vy = 0,
-            ax = 0,
-            ay = 0
-        })
+        x[#x + 1] = math.random() * dim.x
+        y[#y + 1] = math.random() * dim.y
+        vx[#vx + 1] = 0
+        vy[#vy + 1] = 0
+        ax[#ax + 1] = 0
+        ay[#ay + 1] = 0
     end
 end
-function updateParticles(particles, dim, dt)
+function updateParticles(xl, yl, vxl, vyl, axl, ayl, dim, dt)
     local sqrt = math.sqrt
     local clamp = math.clamp
     local movementSpeed = 0.1
-    for k = 1, #particles do
-        local p = particles[k]
-        p.vx = p.vx + p.ax * dt * movementSpeed
-        p.vy = p.vy + p.ay * dt * movementSpeed
-        p.x = p.x + p.vx * dt * movementSpeed
-        p.y = p.y + p.vy * dt * movementSpeed
-        boundObject(p, 0.8, clamp)
+    for i = 1, #xl do
+        local x = xl[i]
+        local y = yl[i]
+        local vx = vxl[i]
+        local vy = vyl[i]
+        local ax = axl[i]
+        local ay = ayl[i]
+        local bounceCoefficient = 0.8
         local sgPosx = dim.x / 2
         local sgPosy = dim.y / 2
-        local xDist = sgPosx - p.x
-        local yDist = sgPosy - p.y
+        local xDist = sgPosx - x
+        local yDist = sgPosy - y
         local dist = sqrt(xDist ^ 2 + yDist ^ 2)
         local sqrtDist = sqrt(dist)
         if (dist < 10) then dist = 10 end
         local gx = xDist / (dist ^ 3) * 500
         local gy = yDist / (dist ^ 3) * 500
-        p.ax = gx
-        p.ay = gy
-        p.ax = p.ax + gy / sqrtDist * 10
-        p.ay = p.ay - gx / sqrtDist * 10
-        p.vx = clamp(p.vx * (1 - dt / 1000 * 2), -5, 5)
-        p.vy = clamp(p.vy * (1 - dt / 1000 * 2), -5, 5)
-    end
-end
-function boundObject(o, bounceCoefficient, clamp)
-    local dim = imgui.GetWindowSize()
-    if (o.x < 0 or o.x > dim.x) then
-        o.vx = -o.vx * bounceCoefficient
-        o.x = clamp(o.x, 1, dim.x - 1)
-    end
-    if (o.y < 0 or o.y > dim.y) then
-        o.vy = -o.vy * bounceCoefficient
-        o.y = clamp(o.y, 1, dim.y - 1)
+        axl[i] = gx + gy / sqrtDist * 10
+        ayl[i] = gy - gx / sqrtDist * 10
+        vxl[i] = vx + ax * dt * movementSpeed
+        vyl[i] = vy + ay * dt * movementSpeed
+        xl[i] = x + vx * dt * movementSpeed
+        yl[i] = y + vy * dt * movementSpeed
+        if (x < 0 or x > dim.x) then
+            vxl[i] = -vxl[i] * bounceCoefficient
+            xl[i] = clamp(xl[i], 1, dim.x - 1)
+        end
+        if (y < 0 or y > dim.y) then
+            vyl[i] = -vyl[i] * bounceCoefficient
+            yl[i] = clamp(yl[i], 1, dim.y - 1)
+        end
+        vxl[i] = clamp(vxl[i] * (1 - dt / 1000 * 2), -5, 5)
+        vyl[i] = clamp(vyl[i] * (1 - dt / 1000 * 2), -5, 5)
     end
 end
 stars = {}
