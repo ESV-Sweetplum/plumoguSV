f364cef491c69874b8000799791a66bf8fc4d9aa
plummyyummy <sweetplumquaver@gmail.com>
2025-08-26 14:57:23 -0700

    more color listing

--- plugin.lua
+++ plugin.lua
@@ -49,8 +49,13 @@ function clock.listen(id, interval)
     return false
 end
 color.vctr.red = vector.New(1, 0, 0, 1)
+color.vctr.orange = vector.New(1, 0.5, 0, 1)
+color.vctr.yellow = vector.New(1, 1, 0, 1)
 color.vctr.green = vector.New(0, 1, 0, 1)
+color.vctr.aqua = vector.New(0, 1, 1, 1)
 color.vctr.blue = vector.New(0, 0, 1, 1)
+color.vctr.purple = vector.New(1, 0, 1, 1)
+color.vctr.magenta = vector.New(1, 0, 0.5, 1)
 color.vctr.white = vector.New(1, 1, 1, 1)
 color.vctr.black = vector.New(0, 0, 0, 1)
 color.vctr.transparent = vector.New(0, 0, 0, 0)
