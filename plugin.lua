0ead799998382f0156feac2559d5fc2b9716ae1a
plummyyummy <sweetplumquaver@gmail.com>
2025-09-20 18:17:45 -0700

    add tooltip thingy

--- plugin.lua
+++ plugin.lua
@@ -9893,6 +9893,8 @@ function showAppearanceSettings()
         globalVars.customStyle = customStyle
         globalVars.colorThemeIndex = table.indexOf(COLOR_THEMES, "CUSTOM")
     end
+    HoverToolTip(
+    "Clicking this will recreate this theme in the CUSTOM theme option, allowing you to customize it however you'd like without having to clone it manually.")
     AddSeparator()
     chooseCursorTrail()
     chooseCursorTrailShape()
