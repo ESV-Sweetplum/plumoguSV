dba7357098a62cbb4d3f8d02bd3e81b82359d60d
plummyyummy <sweetplumquaver@gmail.com>
2026-03-23 18:17:57 -0700

    use or statement

--- packages/table/parse.lua
+++ packages/table/parse.lua
@@ -8,8 +8,8 @@ function table.parse(str)
     if (str == 'FALSE' or str == 'TRUE') then return str == 'TRUE' end
     if (str:charAt(1) == '"') then return str:sub(2, -2) end
     if (str:match('^%-?%d+$') or str:match('^%-?%d+E[%+%-]%d+$')) then return math.toNumber(str) end
-    if (str:match('^%-?%.%d+$') and str:match('^%-?%.%d+E[%+%-]%d+$')) then return math.toNumber(str) end
-    if (str:match('^%-?%d+%.%d+$') and str:match('^%-?%d+%.%d+E[%+%-]%d+$')) then return math.toNumber(str) end
+    if (str:match('^%-?%.%d+$') or str:match('^%-?%.%d+E[%+%-]%d+$')) then return math.toNumber(str) end
+    if (str:match('^%-?%d+%.%d+$') or str:match('^%-?%d+%.%d+E[%+%-]%d+$')) then return math.toNumber(str) end
     if (not table.contains({ '{', '[' }, str:charAt(1))) then
         print('e!',
             'Something really bad has happened with the parsing algorithm weewooweewoo please report this to the Discord thanks!!!!!!!!!')
