6fde8fe7d3c5b70c43e94fe5f003857727a0dfc5
plummyyummy <sweetplumquaver@gmail.com>
2026-03-25 15:33:52 -0700

    update get time desc

--- packages/clock/getTime.lua
+++ packages/clock/getTime.lua
@@ -1,4 +1,4 @@
----Returns the number of milliseconds the plugin has been active.
+---Returns the number of seconds that the plugin has been active.
 ---@return number lifetime
 function clock.getTime()
     return (state.UnixTime - clock.prevTime) / 1000
