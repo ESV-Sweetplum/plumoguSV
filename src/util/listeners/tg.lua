0a4153937143297f68767c444e6e23657df07d59
plummyyummy <sweetplumquaver@gmail.com>
2026-04-09 19:21:52 -0700

    update tg listener

--- src/util/listeners/tg.lua
+++ src/util/listeners/tg.lua
@@ -1,9 +1,17 @@
 function listenForTimingGroupCount()
-    cache.tgList = game.get.timingGroupList()
+    local function refreshTgList()
+        local baseList = game.get.timingGroupList()
+        if (globalVars.hideAutomatic) then
+            table.filter(baseList,
+                function(str) return not string.find(str, 'automate_') end)
+        end
+        cache.tgList = baseList
+    end
 
     listen(function(action, type, fromLua)
         local actionIndex = tonumber(action.Type)
-        if (actionIndex < action_type.CreateTimingGroup and actionIndex ~= action_type.Batch) then return end
-        cache.tgList = game.get.timingGroupList()
+        if (actionIndex < action_type.CreateTimingGroup) then return end
+
+        refreshTgList()
     end)
 end
